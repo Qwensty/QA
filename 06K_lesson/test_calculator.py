3e7c726ef8cfcac0aa40b4a39427471334916110
Ruslan <thefox1999@yandex.ru>
2024-11-10 16:16:09 +0200

    v1 lesson_6K

--- 06K_lesson/test_calculator.py
+++ 06K_lesson/test_calculator.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+#Запись для обновления commit
 # Инициализация веб-драйвера
 driver = webdriver.Chrome()
 
